a48a9756acd9593769adcab88fc74c29eb56e0a1
Jonathan Soares <jonthan.soares10@gmail.com>
2023-03-21 17:16:48 -0300

    testando media query

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Oi, tudo bem?")
+function trocaCor(cor) {
+    let corCirculo = document.querySelector(".circulo")
+    corCirculo.style.background = cor
+}
+
+function trocaImg(imagem) {
+    let corIphone = document.querySelector(".iphone")
+    corIphone.src = imagem
+}
